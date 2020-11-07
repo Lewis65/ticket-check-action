a57a92328b6814def5daed07e4dbd0b43155be4e
Lewis Horwood <horwood.lewis@gmail.com>
2020-11-07 16:48:01 -0700

    update packages, change ncc to maintained version

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 /* eslint-disable @typescript-eslint/camelcase */
 
 import { debug as log, getInput, setFailed } from '@actions/core';
-import { context, GitHub } from '@actions/github';
+import { context, getOctokit } from '@actions/github';
 
 // Helper function to retrieve ticket number from a string (either a shorthand reference or a full URL)
 const extractId = (value: string): string | null => {
@@ -38,7 +38,7 @@ async function run(): Promise<void> {
 
     // Instantiate a GitHub Client instance
     const token = getInput('token', { required: true });
-    const client = new GitHub(token);
+    const client = getOctokit(token);
     const { owner, repo, number } = context.issue;
     const login = context.payload.pull_request?.user.login as string;
     const senderType = context.payload.pull_request?.user.type as string;
